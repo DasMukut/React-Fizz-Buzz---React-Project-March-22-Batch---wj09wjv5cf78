e1d1acc7c824fe347a12298c6afa79f9203d529a
DasMukut <108810054+DasMukut@users.noreply.github.com>
2022-10-15 13:56:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,80 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+// import React from 'react'
+// import '../styles/App.css';
+// const App = () => {
 
+  
+//   return (
+//     <div id="main">
+
+//     </div>
+//   )
+// }
+
+
+// export default App;
+
+
+import React, { useState } from "react";
+import "../styles/App.css";
+const App = () => {
+  const [count, setCount] = useState(1);
+   const [getClass, setClass] = useState("normal");
+  
+  function handleIncre(){
+     setCount(count+1)
+     if((count+1)%3==0 && (count+1)%5!==0 ){
+       setClass("fizz")
+     }else if((count+1)%5==0 && (count+1)%3!==0){
+       setClass("buzz")
+     }else if((count+1)%3==0 && (count+1)%5==0){
+        setClass("fizzbuzz")
+     }else{
+       setClass("normal")
+     }
+  }
+  
+  function handleDecre(){
+     setCount(count-1)
+  if((count-1)%3==0 && (count-1)%5!==0 ){
+       setClass("fizz")
+     }else if((count-1)%5==0 && (count-1)%3!==0){
+       setClass("buzz")
+     }else if((count-1)%3==0 && (count-1)%5==0){
+        setClass("fizzbuzz")
+     }else{
+       setClass("normal")
+     }
+  }
   
   return (
     <div id="main">
+      <h1> hi all </h1>
 
+      <button
+        id="increment" 
+        onClick={handleIncre}
+      >
+        Increment
+      </button>
+
+      <h1 className={getClass}>
+    
+        {count} 
+      </h1>
+
+      <button
+        id="decrement" 
+        onClick={handleDecre}
+      >
+        Decrement
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
+
+
+
+
+
